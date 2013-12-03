12fcecd1aa457b84a8431b392d602b1a295555c1
lgpperry <lgpperry@gmail.com>
2013-12-02 20:58:19 -0800

    Signed-off-by: lgpperry <lgpperry@gmail.com>

---
+++ lp2497135/Llist/Llist.h
@@ -0,0 +1,28 @@
+//Mark E. Lehr
+//Nov 27th, 2013
+//List Specification
+//Minimalist List
+
+
+#ifndef LLIST_H
+#define LLIST_H
+#include "Node.h"
+#include <cstddef>  //Null definition
+#include <cassert>  //Assert function
+
+class Llist{
+    private:
+	Node *first;  //Node Head
+	Node *last;   //Node Tail
+	int count;    //Number of Nodes
+    public:
+	Llist();
+	~Llist();
+	int getLength();         //Number of Nodes
+	Object * fromFront(int); //Return nth Object from the front
+	void insert(Object &);   //Insert an Object
+	void addto(Object &);	 //Add an Object to the end
+};
+
+
+#endif
---
+++ lp2497135/Llist/LlistTest.cpp
@@ -0,0 +1,35 @@
+//Mark E. Lehr
+//Nov 27th, 2013
+//List Implementation Test 
+
+#include <iostream>
+#include "Object.h"
+#include "Llist.h"
+using namespace std;
+
+//GOAL: insert items from the back
+
+int main(int argv,char *argc[]){
+    //Create 3 objects, note I didn't set the
+	//information in the objects.  Not necessary
+	//to perform this test.
+	int n=4;
+	Object info1(n),info2(n+1),info3(n+2),info4(n+3);
+
+	//Insert the 3 objects in the list
+	Llist x;
+	x.insert(info1);
+	x.insert(info2);
+	x.insert(info3);
+	x.addto(info4);
+
+	//Check out the information
+	cout<<"The number of insertions is = "<<x.getLength()<<endl;
+	cout<<"The size of the first object in the List is = " <<x.fromFront(1)->getSize()<<endl;
+	cout<<"The size of the second object in the List is = "<<x.fromFront(2)->getSize()<<endl;
+	cout<<"The size of the third object in the List is = " <<x.fromFront(3)->getSize()<<endl;
+	cout<<"The size of the fourth object in the List is = " <<x.fromFront(4)->getSize()<<endl;
+
+	cin.get();
+	return 1;
+}
